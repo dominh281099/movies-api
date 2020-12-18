8404a2f481f1a7e3c6669e9d32757aeb47a608ca
manh do <dohuuminh281099@gmail.com>
2020-12-18 20:09:20 +0700

    first commit

---
+++ components/header.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+export default function HeaderComponent(props) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="MinhDo" />
+        <meta name="description" content="Search your movies" />
+        <meta name="keywords" content="search film" />
+        <title>{props.title}</title>
+      </Head>
+     
+    </>
+  )
+}
